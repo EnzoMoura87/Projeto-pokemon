fb832606617c9a83ba2cb55ee0c2f1d43d16ef98
Enzo Moura <93396168+EnzoMoura87@users.noreply.github.com>
2022-08-05 20:34:12 -0300

    js

--- src/js/script.js
+++ src/js/script.js
@@ -16,13 +16,32 @@ OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o  cartao ante
 
 // - passo 1 - dar um jeito de pegar o elemento HTML da seta avançar
 const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+const cartoes = document.querySelectorAll(".cartao");
 let cartaoAtual = 0;
 
 // - passo 2 - dar um jeito de identificar o clique do usuario na seta avançar
 
 btnAvancar.addEventListener("click", function () {
-  // - passo 3 - fazer aparecer o proximo cartao da lista
-  const cartoes = document.querySelectorAll(".cartao");
+  if (cartaoAtual === cartoes.length - 1) return;
+ 
+  //   - passo 4 - buscar o cartao que esta selecionado e esconder
+  const cartaoSelecionado = document.querySelector(".selecionado");
+  cartaoSelecionado.classList.remove("selecionado");
+
   cartaoAtual++;
+  cartoes[cartaoAtual].classList.add("selecionado");
+});
+
+
+// Voltar
+
+btnVoltar.addEventListener("click", function () {
+    if(cartaoAtual === 0) return;
+
+  const cartaoSelecionado = document.querySelector(".selecionado");
+  cartaoSelecionado.classList.remove("selecionado");
+
+  cartaoAtual--;
   cartoes[cartaoAtual].classList.add("selecionado");
 });
